b6512f6af44849824e8bd96baacdd04dfc6dacda
love singh <love singh@DESKTOP-UE11KPR>
2019-03-20 15:17:33 +0530

    Calculator Project Done

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -39,18 +39,20 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
-    	
+    	result=x+y;
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
 //        write your code here
-        return result;
+    	result = x-y;
+    	return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
+    	result = x*y;
 //      write your code here        
     	return result;
     }
@@ -58,6 +60,7 @@ public class Calculator {
     {
     	int result=0;
 //      write your code here
+    	result = x/y;
     	return result;
     }
 
